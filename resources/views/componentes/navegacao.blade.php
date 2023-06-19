921d6e27a98ece0ac4f53f97db26f16791e4fa18
Joaopgoulart10 <joaopgoulart10@gmail.com>
2023-06-19 01:02:50 -0300

    front-end

---
+++ resources/views/componentes/navegacao.blade.php
@@ -0,0 +1,30 @@
+<nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-body-tertiary sidebar collapse">
+    <div class="position-sticky pt-3 sidebar-sticky">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
+              <svg class="bi"><use xlink:href="#house-fill"/></svg>
+              Dashboard
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link d-flex align-items-center gap-2" href="#">
+              <svg class="bi"><use xlink:href="#file-earmark"/></svg>
+              Venda
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link d-flex align-items-center gap-2" href="#">
+              <svg class="bi"><use xlink:href="#cart"/></svg>
+              Produtos
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link d-flex align-items-center gap-2" href="#">
+              <svg class="bi"><use xlink:href="#people"/></svg>
+              Clientes
+            </a>
+          </li>
+        </ul>
+    </div>
+</nav>
